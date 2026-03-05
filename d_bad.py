3f43b3fdd9b819771562058ce40f13bd2446d9f9
argonaultes <gael@argonaultes.fr>
2026-03-05 11:11:20 +0100

    dependency good

--- d_bad.py
+++ d_bad.py
@@ -1,6 +1,6 @@
 class FXConverter:
 
-    def convert(self, from_currency, to_currency,amount):
+    def convert(self, from_currency, to_currency, amount):
         print(f'{amount} {from_currency} = {amount * 1.2},{to_currency}')
         return amount * 1.2
 
