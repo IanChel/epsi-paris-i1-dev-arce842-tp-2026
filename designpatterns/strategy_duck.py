8ac61d43643c456ce0e79b88ab579cc008e8fecb
IanChel <sofchel2002@gmail.com>
2026-03-06 09:44:52 +0100

    WeatherStation

--- designpatterns/strategy_duck.py
+++ designpatterns/strategy_duck.py
@@ -1,5 +1,21 @@
 from abc import ABC, abstractmethod, abstractclassmethod
 
+class QuackBehavior(ABC):
+    
+    @abstractmethod
+    def quack(self):
+        ...
+
+class QuackQuack(QuackBehavior):
+    
+    def quack(self):
+        print('Quack quack')
+        
+class SmallQuack(QuackBehavior):
+    
+    def quack(self):
+        print('Small quack')
+
 class FlyBehavior(ABC):
     
     @abstractclassmethod
@@ -21,46 +37,52 @@ class FlyBackward(FlyBehavior):
 
 class Duck(ABC):
     
-    def __init__(self, fly_behavior:FlyBehavior):
+    def __init__(self, fly_behavior:FlyBehavior, quack_behavior:QuackBehavior):
+        self.__fly_behavior = fly_behavior
+        self.__quack_behavior = quack_behavior
+    
+    def display(self):
+        print(self.__class__.__name__, 'says hello')
+    
+    def set_fly_behavior(self, fly_behavior:FlyBehavior):
         self.__fly_behavior = fly_behavior
     
-    @abstractmethod
-    def display(self):
-        ...
-        
     def fly(self):
         self.__fly_behavior.fly()
+        
+    def set_quack_behavior(self, quack_behavior:QuackBehavior):
+        self.__quack_behavior = quack_behavior
+        
+    def quack(self):
+        self.__quack_behavior.quack()
 
     
 class GreenDuck(Duck):
     
     def __init__(self):
-        super().__init__(fly_behavior=FlyToTheMoon())
-
-    def display(self):
-        print('Greenduck says hello')
-    
-    def quack(self):
-        print('Quack quack')
+        super().__init__(fly_behavior=FlyToTheMoon, quack_behavior=QuackQuack())
     
 class MallardDuck(Duck):
 
     def __init__(self):
-        super().__init__(fly_behavior=FlyBackward())
-
-    def display(self):
-        print('MallardDuck says hello')
-    
-    def quack(self):
-        print('Small quack')
+        super().__init__(fly_behavior=FlyBackward, quack_behavior=SmallQuack())
 
 class OtherDuck(Duck):
 
     def __init__(self):
-        super().__init__(fly_behavior=FlyToTheMoon())
+        super().__init__(fly_behavior=FlyToTheMoon, quack_behavior=SmallQuack())
+
+if __name__ == '__main__':
+    green_duck = GreenDuck()
+    green_duck.display()
+    green_duck.fly()
+    green_duck.quack()
+    green_duck.set_fly_behavior(FlyBackward)
+    green_duck.fly()
     
-    def display(self):
-        print('OtherDuck says hello')
-        
-    def quack(self):
-        print('Small quack')
+    mallard = MallardDuck()
+    mallard.display()
+    mallard.fly()
+    mallard.quack()
+    mallard.set_quack_behavior(QuackQuack())
+    mallard.quack()
